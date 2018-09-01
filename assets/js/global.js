9bde0c844ed6a2e557c6df1014d55a53a7512821
Patricio <chazarreta.patricio@gmail.com>
2018-09-01 14:45:40 -0300

    Hechos los inputs para zona y dirección (faltaría getter y setter de php). Sus valores almacenados en localStorage.

--- assets/js/global.js
+++ assets/js/global.js
@@ -7,4 +7,52 @@ $(document).ready(function(){
     $("li").mouseleave(function(event){
         $(event.target).removeClass("active");
     });
+
+    //Elegir locación
+    $locationForm = $('#location-form');
+
+        //Zona
+   /* if (localStorage.getItem('zonaIngresada')) {
+
+        $("#onchange option").eq(localStorage.getItem('zonaIngresada')).prop('selected', true);
+    }*/
+
+    $locationForm.on('submit', function(){
+        localStorage.setItem('zonaIngresada', $('#onchange').val());
+    });
+/*
+    $("#onchange").on('change', function() {
+        localStorage.setItem('zonaIngresada', $('option:selected', this).index());
+    });
+    */
+
+        //Dirección
+    var $addressInput = $('#address-input'); 
+
+    /*if(localStorage.getItem('direccionIngresada')){
+        $addressInput.val(localStorage.getItem('direccionIngresada'));
+    }*/
+
+    $locationForm.on('submit', function(){
+        var direccionIngresada = $addressInput.val();
+        localStorage.setItem('direccionIngresada', direccionIngresada);
+    });
+    
+    
+
+    
+/*
+    $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address=miguel+de+salcedo+,1485,Morón',
+    function(data) {
+        console.log(data);
+    //data is the JSON string
+
 });
+
+*/
+
+    
+});
+
+
+
